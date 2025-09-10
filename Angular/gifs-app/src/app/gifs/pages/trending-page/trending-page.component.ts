ba0d767d636904fa94e9e912c2a2ed588eb98480
Stalin Rivera Vega <stalinrivera1980@gmail.com>
2025-09-09 23:40:25 -0500

    Proyecto de gifs

---
+++ Angular/gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-page',
+  templateUrl: './trending-page.component.html',
+  styleUrls: []
+})
+export default class TrendingPageComponent  {
+
+}
