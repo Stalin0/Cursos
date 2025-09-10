3964f8f76799051601ea64622a846d05811db1bd
Stalin Rivera Vega <stalinrivera1980@gmail.com>
2025-09-09 23:40:25 -0500

    Proyecto de gifs

---
+++ Angular/gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page',
+  templateUrl: './search-page.component.html',
+  styleUrls: []
+})
+export default class SearchPageComponent {
+}
